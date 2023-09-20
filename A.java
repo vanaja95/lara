c5b796c7249238595aa486b41693f24b55c6ecf8
vanaja95 <vanajareddy277@gamail>
2023-09-19 17:00:50 -0700

    vanaja

---
+++ A.java
@@ -0,0 +1,13 @@
+class A 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("iam in lara");
+		System.out.println("iam in lara");
+		System.out.println("iam in lara");
+		System.out.println("iam in lara");
+		System.out.println("iam in lara");
+		System.out.println("iam in lara");
+		System.out.println("iam in lara");
+	}
+}
